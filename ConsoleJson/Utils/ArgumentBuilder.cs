3cacec857a82faac842bdaf9066a044efbbba698
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2023-10-27 23:56:20 +0700

    Add Utils

---
+++ ConsoleJson/Utils/ArgumentBuilder.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleJson.Helpers;
+using ConsoleJson.Models;
+
+namespace ConsoleJson.Utils
+{
+    public class ArgumentBuilder
+    {
+        public Arguments myArguments;
+
+        public ArgumentBuilder(string[] args)
+        {
+            myArguments = BuildArguments(args);
+        }
+
+        private Arguments BuildArguments(string[] args)
+        {
+            if(args != null && args.Length == 3)
+            {
+                                
+                myArguments = new()
+                {
+                    InputPath = args[0],
+                    Country = args[1],
+                    OutputPath = args[2]
+                };
+
+                if(myArguments.Country.Equals(CountryValue.RUS) || myArguments.Country.Equals(CountryValue.USA))
+                {
+                    return myArguments;
+                }
+
+                return null;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ConsoleJson/Utils/OrderLogger.cs
@@ -0,0 +1,31 @@
+﻿
+namespace ConsoleJson.Utils
+{
+    public class OrderLogger
+    {
+        private const string path = "log.txt";
+
+        public static readonly OrderLogger Instance = new OrderLogger();
+
+        private OrderLogger() { }
+
+        public static OrderLogger GetInstance() { return Instance; }
+
+        public void Log(string jsonOrder)
+        {
+            Console.WriteLine(jsonOrder);
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    sw.WriteLine(DateTime.Now.ToString() + "\n" + jsonOrder);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString() + " Не удалось записать лог в файл");
+            }
+        }
+    }
+}
